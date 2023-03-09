706aaec185c72b3fc9898d18c1231b377b9d5924
Artur Orłowski <63999662+aorlowski91@users.noreply.github.com>
2023-03-09 20:41:40 +0100

    Add files via upload

--- js/slick.js
+++ js/slick.js
@@ -1,25 +1,10 @@
-$('.center').slick({
-    centerMode: true,
-    centerPadding: '60px',
+$(document).ready(function(){
+  $('.gallery__box').slick({
     slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    accessibility: true,
   });
+});
+
